46e3455ea327cf0e667ceb595920190c7418371e
Justin Patzer <jepatzer@ncsu.edu>
2016-10-05 10:38:25 -0400

    Initial Project creation, initial skeleton built

---
+++ GameProject/Manager.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class Manager
+    {
+        private Texture2D background;
+        private Texture2D gameOverScreen;
+        //player sprites
+        private Texture2D playerSprite;
+
+        private List<Object> objects;
+        private Object player;
+
+        /// <summary>
+        /// Width of the game screen.
+        /// </summary>
+        private int width;
+        /// <summary>
+        /// Height of the game screen.
+        /// </summary>
+        private int height;
+
+        private bool gameOver;
+        private int playerBufferSpace;
+
+        private Random r;
+
+        public Manager()
+        {
+            objects = new List<Object>();
+            height = 0;
+            width = 0;
+            player = null;
+            r = new Random();
+            gameOver = false;
+            playerBufferSpace = 100;
+        }
+
+        public void initialize(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+        }
+
+        public void loadContent(ContentManager content)
+        {
+            //<variable> = content.Load<Texture2D>(<filename - extension>);
+            //e.g. gameOverScreen = content.Load<Texture2D>("GameOver");
+        }
+
+        public void update(GameTime gameTime, KeyboardState state, KeyboardState previousState)
+        {
+
+            for (int i = objects.Count - 1; i >= 0; i--)
+            {
+                objects[i].update(gameTime, width, height);
+            }
+
+            //player.processInput(state, previousState, gameTime);
+            player.update(gameTime, width, height);
+
+        }
+
+        public void add(Object o)
+        {
+            objects.Add(o);
+        }
+
+        public void draw(SpriteBatch sb)
+        {
+            if (!gameOver)
+            {
+                sb.Draw(background, destinationRectangle: new Rectangle(0, 0, width, height));
+                foreach (Object o in objects)
+                {
+                    o.draw(sb);
+                }
+
+                player.draw(sb);
+            }
+            else
+            {
+                sb.Draw(gameOverScreen, destinationRectangle: new Rectangle(0, 0, width, height));
+            }
+        }
+    }
+}
---
+++ GameProject/Object.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    abstract class Object
+    {
+        private Texture2D sprite;
+        private Vector2 origin;
+        private Vector2 position;
+
+        private double facing;
+
+        public Object(Texture2D sprite, Vector2 position)
+        {
+            this.sprite = sprite;
+            origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+
+            this.position = position - origin;
+            facing = 0;
+            Speed = Vector2.Zero;
+            Radius = 0;
+        }
+
+        /// <summary>
+        /// Should be called when the state of the game updates. Independent from processInput.
+        /// </summary>
+        public virtual void update(GameTime gameTime, int width, int height)
+        {
+            move(gameTime, width, height);
+        }
+
+        private void move(GameTime gameTime, int width, int height)
+        {
+            Position += Vector2.Multiply(Speed, (float)gameTime.ElapsedGameTime.TotalSeconds);
+        }
+
+        /// <summary>
+        /// Call during draw step. Draws the character.
+        /// </summary>
+        /// <param name="sb">
+        /// The spritebatch for drawing
+        /// </param>
+        public virtual void draw(SpriteBatch sb)
+        {
+            sb.Draw(sprite,
+            destinationRectangle: new Rectangle((int)(origin.X + position.X), (int)(origin.Y + position.Y), sprite.Width, sprite.Height),
+            origin: origin,
+            rotation: (float)facing);
+        }
+
+        // Variables
+
+        public Vector2 Position {
+            get { return position + origin; }
+            protected set { position = value - origin; }
+        }
+
+        public double Radius {
+            get;
+            protected set;
+        }
+
+        public Vector2 Speed
+        {
+            get;
+            protected set;
+        }
+    }
+}
