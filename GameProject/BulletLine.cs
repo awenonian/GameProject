e830c80d5db021c2af19ce01787752363c0c1857
Justin Patzer <jepatzer@ncsu.edu>
2016-10-24 11:56:20 -0400

    built the BulletLine class, with collision detection on rectangles

---
+++ GameProject/BulletLine.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class BulletLine
+    {
+        private Vector2 origin;
+        private float rotation;
+
+        private Vector2 direction;
+
+        private Texture2D line;
+
+        public BulletLine(Vector2 origin, float rotation)
+        {
+            this.origin = origin;
+            this.rotation = rotation;
+
+            direction = new Vector2((float) Math.Cos(rotation), (float) Math.Sin(rotation));
+        }
+
+        public bool isColliding(Rectangle rect)
+        {
+            // If you're behind the bullet
+            if (direction.X < 0)
+            {
+                if (rect.X > origin.X)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (rect.X + rect.Width < origin.X)
+                {
+                    return false;
+                }
+            }
+
+            // If the top of the rectangle is below the line
+            if (rect.Y < origin.Y + (origin.X - rect.X) * direction.Y && rect.Y < origin.Y + (origin.X - (rect.X + rect.Width)) * direction.Y)
+            {
+                return false;
+            }
+            // If the bottom of the rectangle is above the line
+            if (rect.Y + rect.Height > origin.Y + (origin.X - rect.X) * direction.Y && rect.Y + rect.Height > origin.Y + (origin.X - rect.Width) * direction.Y)
+            {
+                return false;
+            }
+
+            // Otherwise, we're colliding
+            return true;
+        }
+
+        public void draw(SpriteBatch sb)
+        {
+            sb.Draw(line, origin, new Rectangle(0, 0, width, height), Color.White, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        }
+
+    }
+}
