6c6101ddd249e9ce140a199b0b64348ca953f59d
Justin Patzer <jepatzer@ncsu.edu>
2016-11-14 11:33:31 -0500

    Implemented Plan constructor

---
+++ GameProject/Plan.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class Plan
+    {
+        private BulletLine[] shots;
+
+        public Plan(Vector2 origin, int count, float startAngle, float endAngle, bool regularSpread, Manager manager)
+        {
+            Random rand = new Random();
+            shots = new BulletLine[count];
+            for (int i = 0; i < count; i ++)
+            {
+                if (regularSpread) {
+                    shots[i] = new BulletLine(origin, startAngle + i * (endAngle - startAngle), manager);
+                }
+                else
+                {
+                    shots[i] = new BulletLine(origin, (float) rand.NextDouble() * (endAngle - startAngle) + startAngle, manager);
+                }
+            }
+        }
+
+    }
+}
